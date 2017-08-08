97f3f58541720fd1a5ae3aa3301beef0b6c1c9ba
邰广银 <273650669@qq.com>
2017-08-08 08:36:37 +0800

    去掉无用的引用

--- src/main.js
+++ src/main.js
@@ -11,9 +11,6 @@ import VueI18n from 'vue-i18n';
 import Locales from './locale';
 import zhLocale from 'iview/src/locale/lang/zh-CN';
 import enLocale from 'iview/src/locale/lang/en-US';
-import 'animate.css'
-import 'vue-fullpage/vue-fullpage.css'
-import VueFullpage from 'vue-fullpage'
 
 Vue.use(VueFullpage);
 Vue.use(VueRouter);
