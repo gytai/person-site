3b72f23ff7cfbd433beb5317133181e52f768b00
邰广银 <273650669@qq.com>
2017-08-12 10:54:33 +0800

    fix bugs

--- src/views/product.vue
+++ src/views/product.vue
@@ -4,12 +4,77 @@
     }
 
     .card{
-        float: left;
-        margin: 20px;
+        margin: 0 auto;
+        margin-top: 20px;
+    }
+
+    .container{
+        padding-left: 20px;
+        padding-right: 20px;
     }
 </style>
 <template>
     <div class="container">
+        <Card class="card" style="width:100%;">
+            <p slot="title">
+                <Icon type="ios-film-outline"></Icon>
+                乐房通
+            </p>
+            <Carousel>
+                <Carousel-item>
+                    <div class="demo-carousel"><img src="../images/product/p1-1.png" /></div>
+                </Carousel-item>
+                <Carousel-item>
+                    <div class="demo-carousel"><img src="../images/product/p1-2.png" /></div>
+                </Carousel-item>
+                <Carousel-item>
+                    <div class="demo-carousel"><img src="../images/product/p1-3.png" /></div>
+                </Carousel-item>
+                <Carousel-item>
+                    <div class="demo-carousel"><img src="../images/product/p1-4.png" /></div>
+                </Carousel-item>
+            </Carousel>
+        </Card>
+        <Card class="card" style="width:100%;">
+            <p slot="title">
+                <Icon type="ios-film-outline"></Icon>
+                雅集诗词官网
+            </p>
+            <Carousel>
+                <Carousel-item>
+                    <div class="demo-carousel"><img src="../images/product/p6-1.png" /></div>
+                </Carousel-item>
+                <Carousel-item>
+                    <div class="demo-carousel"><img src="../images/product/p6-2.png" /></div>
+                </Carousel-item>
+                <Carousel-item>
+                    <div class="demo-carousel"><img src="../images/product/p6-3.png" /></div>
+                </Carousel-item>
+                <Carousel-item>
+                    <div class="demo-carousel"><img src="../images/product/p6-4.png" /></div>
+                </Carousel-item>
+            </Carousel>
+        </Card>
+        <Card class="card" style="width:100%;">
+            <p slot="title">
+                <Icon type="ios-film-outline"></Icon>
+                造物者
+            </p>
+            <Carousel>
+                <Carousel-item>
+                    <div class="demo-carousel"><img src="../images/product/p5-1.png" /></div>
+                </Carousel-item>
+                <Carousel-item>
+                    <div class="demo-carousel"><img src="../images/product/p5-2.png" /></div>
+                </Carousel-item>
+                <Carousel-item>
+                    <div class="demo-carousel"><img src="../images/product/p5-3.png" /></div>
+                </Carousel-item>
+                <Carousel-item>
+                    <div class="demo-carousel"><img src="../images/product/p5-4.png" /></div>
+                </Carousel-item>
+            </Carousel>
+        </Card>
         <Card class="card" style="width:350px;height: 650px;">
             <p slot="title">
                 <Icon type="ios-film-outline"></Icon>
@@ -73,57 +138,8 @@
                 </Carousel-item>
             </Carousel>
         </Card>
-        <Card class="card" style="width:350px;height: 250px;">
-            <p slot="title">
-                <Icon type="ios-film-outline"></Icon>
-                乐房通
-            </p>
-            <Carousel>
-                <Carousel-item>
-                    <div class="demo-carousel"><img src="../images/product/p1-1.png" /></div>
-                </Carousel-item>
-                <Carousel-item>
-                    <div class="demo-carousel"><img src="../images/product/p1-2.png" /></div>
-                </Carousel-item>
-                <Carousel-item>
-                    <div class="demo-carousel"><img src="../images/product/p1-3.png" /></div>
-                </Carousel-item>
-                <Carousel-item>
-                    <div class="demo-carousel"><img src="../images/product/p1-4.png" /></div>
-                </Carousel-item>
-            </Carousel>
-        </Card>
-        <Card class="card" style="width:350px;height: 250px;">
-            <p slot="title">
-                <Icon type="ios-film-outline"></Icon>
-                造物者
-            </p>
-            <Carousel>
-                <Carousel-item>
-                    <div class="demo-carousel"><img src="../images/product/p5-1.png" /></div>
-                </Carousel-item>
-                <Carousel-item>
-                    <div class="demo-carousel"><img src="../images/product/p5-2.png" /></div>
-                </Carousel-item>
-                <Carousel-item>
-                    <div class="demo-carousel"><img src="../images/product/p5-3.png" /></div>
-                </Carousel-item>
-                <Carousel-item>
-                    <div class="demo-carousel"><img src="../images/product/p5-4.png" /></div>
-                </Carousel-item>
-            </Carousel>
-        </Card>
-        <Card class="card" style="width:80%;height: 250px;">
-            <p slot="title">
-                <Icon type="ios-film-outline"></Icon>
-                更多...
-            </p>
-           <p>
-               这里就不一一列举了。作为一个程序猿，要学习的有很多。我也将继续加油。<br>
-               写这个网站主要是练习Vue和iView 这个前端框架。
-               谢谢大家。
-           </p>
-        </Card>
+
+
     </div>
 </template>
 
@@ -137,7 +153,7 @@
             }
         },
         mounted(){
-
+            this.$Message.success('每个作品可切换，查看多张效果图哦');
         }
     }
 
